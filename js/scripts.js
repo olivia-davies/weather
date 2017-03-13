73a31961940199c9b854e5dfea60844bccdd19e5
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-03-13 14:11:50 -0700

    Added Simple Weather plugin
    *added Js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,28 @@
 // YOUR SCRIPTS GO HERE
+
+// http://simpleweatherjs.com/
+
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+
+$.simpleWeather({
+  location: 99004,
+  
+  unit: 'f',
+  
+  success: function(weather){
+    // Check for weather
+    console.log(weather);
+    
+    // Display Weather
+    $('.temp').text(weather.temp);
+    $('.city').text(weather.city)
+    $('.state').text(weather.region)
+    $('.image img').attr('src', weather.image);
+    
+    
+  },
+  
+  error: function(error) {
+    console.log('Mira afuera.');
+  }
+});
