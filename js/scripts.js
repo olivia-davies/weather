c1fdf0d7cf4ff9966018789ccd877c0069c2979e
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-03-23 21:02:54 -0700

    Giant UPDATE
    *FINALLY got weather icons to work. Now i need to write javascript
    *added gradient bg
    *added fonts for weather
    *decided basic layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,52 @@
 // YOUR SCRIPTS GO HERE
 
-// http://simpleweatherjs.com/
-
 // https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+// https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js
+
+
 
 $.simpleWeather({
-  location: 99004,
-  
-  unit: 'f',
-  
-  success: function(weather){
-    // Check for weather
-    console.log(weather);
-    
-    // Display Weather
-    $('.temp').text(weather.temp);
-    $('.city').text(weather.city)
-    $('.state').text(weather.region)
-    $('.image img').attr('src', weather.image);
-    
-    
-  },
-  
-  error: function(error) {
-    console.log('Mira afuera.');
-  }
+    location: 99004,
+    unit: 'f',
+    success: function (weather) {
+        // Entire weather object
+        console.log(weather);
+
+
+        // Change Icon w/ Condition code
+        var iconCode = 'wi-' + weather.currently;
+
+        //$('i').attr('class', iconCode);
+
+        // Display Data
+
+        $('.geo').text(weather.city);
+        $('.temp').text(weather.temp);
+        $('iconCode').addClass(weather.currently);
+
+    },
+    error: function (error) {
+        // Show if weather cannot be retreived
+        console.log('Look outside.');
+    }
+
 });
+
+
+// Get and store Geo Location lat/long coordinates
+navigator.geolocation.getCurrentPosition(
+
+    function (position) {
+
+        // wait a few seconds to receive location
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        console.log(lat, long);
+
+
+    });
+
+
+
+
