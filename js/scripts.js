387823e4791a4cb66f7cfe56057fa63c087b9a27
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-03-24 13:29:57 -0700

    Added some base weather classes
    * starting to make a loading page
    * worked on condition code :)

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 // https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
 // https://cdnjs.cloudflare.com/ajax/libs/sidr/2.2.1/jquery.sidr.min.js
 
+
 // Get Geo Location
 // Check for GeoLocation Support on Browser
 if ('geolocation' in navigator) {
@@ -15,6 +16,8 @@ if ('geolocation' in navigator) {
     $('.geo').prepend('<p>Geolocation Not Supported</p>');
 
 }
+        
+
 
 // Call Simple Weather
 var getWeather = function (location) {
@@ -31,20 +34,35 @@ var getWeather = function (location) {
             $('.geo').text(weather.city);
             $('.temp').text(weather.temp);
 
-            // Change Icon w/ Condition code & Diplay Icon Data
-            $('i').attr('class', iconCode);
-
+            // Icon Var
             var iconCode = 'wi wi-yahoo-' + weather.code;
             $('i').attr('class', iconCode);
 
+            // Change Icon w/ Condition code & Diplay Data
+            $('i').attr('class', iconCode);
+    
+
+
+
+            //Condition C0D3
+
+            if (weather.code === 9 || weather.code === 18 || weather.code === 35 || weather.code === 45 || weather.code === 47 || weather.code <= 5 && weather.code >= 7 || weather.code <= 11 && weather.code >= 12 || weather.code <= 37 && weather.code >= 40) {
+                console.log('SAH DU');
+
+                $('body').addClass('rainy');
+                $('.cusswerds').h1('When the frick frack did it start raining?!');
+            }
+
+            if (weather.code == 8 || weather.code == 10 || weather.code == 37) {
+                $('body').addClass('hail');
+                $('.cusswerds').h1('What the HAIL is going on out there?');
+            }
         },
-
-
-        error: function (error) {
+                error: function (error) {
             // Show if weather cannot be retreived
-            alert("For crying out loud. Just look outside");
+            alert("For crying out loud. Just look outside, dodge ram-it");
         }
-
+        
     });
 };
 
